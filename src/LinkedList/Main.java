047690121adb1fa448ddfb74d37b2fe40a04b2f1
David Prieto <david.s.prieto1@gmail.com>
2022-10-27 19:55:31 -0500

    Started udemy data structures and algorithms course

---
+++ src/LinkedList/Main.java
@@ -0,0 +1,25 @@
+package LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Practice
+        LinkedList myLinkedList = new LinkedList(4);
+
+        myLinkedList.getHead();
+        myLinkedList.getTail();
+        myLinkedList.getLength();
+
+        myLinkedList.printList();
+
+        // Practice
+        LinkedList myLinkedList1 = new LinkedList(1);
+
+        myLinkedList1.append(2);
+
+        myLinkedList1.printList();
+
+    }
+
+}
