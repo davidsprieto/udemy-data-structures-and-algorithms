fccce3aae34f22e34356046110ba2eef8237fff6
David Prieto <david.s.prieto1@gmail.com>
2022-10-31 17:14:33 -0500

    Completed section #4 - #28

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -25,12 +25,68 @@ public class LinkedList {
         Node newNode = new Node(value);
         if (length == 0) {
             head = newNode;
-            tail = newNode;
         } else {
             tail.next = newNode;
+        }
+        tail = newNode;
+        length++;
+    }
+
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
             tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
         }
         length++;
+    }
+
+    public Node removeLast() {
+        if (length == 0) {
+            return null;
+        }
+        Node temp = head;
+        Node pre = head;
+        while (temp.next != null) {
+            pre = temp;
+            temp = temp.next;
+        }
+        tail = pre;
+        tail.next = null;
+        length--;
+        if (length == 0) {
+            head = null;
+            tail = null;
+        }
+        return temp;
+    }
+
+    public Node removeFirst() {
+        if (length == 0) {
+            return null;
+        }
+        Node temp = head;
+        head = head.next;
+        temp.next = null;
+        length--;
+        if (length == 0) {
+            tail = null;
+        }
+        return temp;
+    }
+
+    public Node get(int index) {
+        if (index < 0 || index >= length) {
+            return null;
+        }
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
     }
 
     public void getHead() {
--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -4,21 +4,64 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Practice
-        LinkedList myLinkedList = new LinkedList(4);
+        // Practice - Create a new linked list object and print it
+//        LinkedList myLinkedList = new LinkedList(4);
+//
+//        myLinkedList.getHead();
+//        myLinkedList.getTail();
+//        myLinkedList.getLength();
+//
+//        myLinkedList.printList();
 
-        myLinkedList.getHead();
-        myLinkedList.getTail();
-        myLinkedList.getLength();
+        // Practice - append a new node to a linked list
+//        LinkedList myLinkedList1 = new LinkedList(1);
+//
+//        myLinkedList1.append(2);
+//
+//        myLinkedList1.printList();
 
+        // Practice - remove last node of a linked list
+//        LinkedList myLinkedList = new LinkedList(1);
+//        myLinkedList.append(2);
+//
+//        myLinkedList.printList();
+
+//        // (2) Items - Returns 2 Node
+//        System.out.println(myLinkedList.removeLast().value);
+//        // (1) Item - Returns 1 Node
+//        System.out.println(myLinkedList.removeLast().value);
+//        // (0) Items - Returns null
+//        System.out.println(myLinkedList.removeLast());
+
+        // Practice prepend a new node to a linked list
+//        LinkedList myLinkedList = new LinkedList(2);
+//        myLinkedList.append(3);
+//
+//        myLinkedList.prepend(1);
+//
+//        myLinkedList.printList();
+
+        // Practice - remove first node from a linked list
+//        LinkedList myLinkedList = new LinkedList(1);
+//        myLinkedList.prepend(2);
+//        myLinkedList.printList();
+//
+//        // (2) Items - Returns 2 Node
+//        System.out.println(myLinkedList.removeFirst().value);
+//        // (1) Item - Returns 1 Node
+//        System.out.println(myLinkedList.removeFirst().value);
+//        // (0) Items - Returns null
+//        System.out.println(myLinkedList.removeFirst());
+
+        // Practice - Return a node at a specified index
+        LinkedList myLinkedList = new LinkedList(0);
+        myLinkedList.append(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
         myLinkedList.printList();
 
-        // Practice
-        LinkedList myLinkedList1 = new LinkedList(1);
+        System.out.println("Got the node: " + myLinkedList.get(2).value);
 
-        myLinkedList1.append(2);
-
-        myLinkedList1.printList();
 
     }
 
